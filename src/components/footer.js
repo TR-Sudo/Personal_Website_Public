9eed637820ab53834a7e5956a7819418ec6ad271
TR-Sudo <78048789+TR-Sudo@users.noreply.github.com>
2023-02-12 15:57:42 -0500

    Styling changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,15 @@ export default function Footer(){
     function toGithub(){
         window.location.href="https://github.com/TR-Sudo"
     }
+    function mailTo(){
+        window.location.href="mailto:T.rashid489@gmail.com"
+    }
     return(
         <footer id="Contact">
             <h1>© 2022 Created Using React JS ( Email T.rashid489@gmail.com )</h1>
             <div className="iconfooter">
                 <img className="imgfooter" onClick={toLinkedin} src='../images/linkedin.png' href="https://www.linkedin.com/in/talha-rashid-489tr/" alt="Linkedin"/>
+                <img className="imgfooter" onClick={mailTo} src='../images/gmail.png' href="mailto:T.rashid489@gmail.com" alt="Github"/>
                 <img className="imgfooter" onClick={toGithub} src='../images/github.png' href="https://github.com/TR-Sudo" alt="Github"/>
             </div>
             <h1>Talha Al Rashid</h1>
